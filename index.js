4df31b62f991daecb80b81efd947420dbc5a4150
abdulladone94 <abdulladone94@gmail.com>
2021-11-17 09:26:21 +0530

    Store variables in the db

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const express = require("express");
 const mysql = require("mysql");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password",
   database: "employeesystem",
+});
+
+// db.connect((err, details)=>{
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(details)
+//   }
+// });
+
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
+
+  db.query(
+    "INSERT INTO employee (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
 });
 
 app.listen(3001, () => {
